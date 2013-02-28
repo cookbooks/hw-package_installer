b23e7f38cde58e8744b0af4410970fde77117654
Brandon CS Sanders <github.b@thesanders.us>
2013-02-28 13:06:13 -0800

    Ensure that variables are defined before they are used

--- recipes/default.rb
+++ recipes/default.rb
@@ -1,11 +1,9 @@
 # Iterate and install packages
 node[:package_installer][:packages].each do |pkg_name, pkg_info|
-  if(pkg_info)
-    pkg_action = pkg_info[:action] || :install
-    pkg_version = pkg_info[:version]
-  end
+  pkg_action  = (pkg_info && pkg_info[:action])
+  pkg_version = (pkg_info && pkg_info[:version])
   package pkg_name do
-    action pkg_action
+    action (pkg_action || :install)
     version pkg_version if pkg_version
   end
 end
